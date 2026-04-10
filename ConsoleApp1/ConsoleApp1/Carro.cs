8ec109213a5442444ab3dfc707943f2d867598bc
KarolinyMenezes <karoliny.menezess@gmail.com>
2026-04-09 22:39:03 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Carro.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Carro
+    {
+        private string _motor;
+        private string _rodas;
+        private string _volante;
+        private string _marca;
+        public Carro()
+        {
+            this._motor = null;
+            this._rodas = null;
+            this._volante = null;
+            this._marca = null;
+        }
+        public Carro(string motor, string rodas, string volante, string assentos)
+        {
+            this._motor = null;
+            this._rodas = null;
+            this._volante = null;
+            this._marca = null;
+        }
+        public string get_motor ()
+        {
+            return this._motor; 
+        }
+        public void set_motor(string motor)
+        {
+            this._motor = motor;
+        }
+        public string get_rodas ()
+        {
+            return this._rodas;
+        }
+        public void set_rodas(string rodas)
+        {
+            this._rodas = rodas;
+        }
+        public string get_volante()
+        {
+            return this._volante; 
+        }
+        public void set_volante(string volante)
+        {
+            this._volante = volante;
+        }
+        public string get_marca()
+        {
+            return this._marca;
+        }
+        public void set_assentos(string assentos)
+        {
+            this._marca = assentos;
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Carro c = new Carro();
+
+Console.WriteLine("Qual a marca?");
+c.set_motor(Console.ReadLine());
+
+Console.WriteLine("Quais as rodas?");
+c.set_rodas(Console.ReadLine());
+
+Console.WriteLine("Qual o volante?");
+c.set_volante(Console.ReadLine());
+
+Console.WriteLine("Qual o motor?");
+c.set_volante(Console.ReadLine());
+
+Console.WriteLine(string.Concat("Seu carro é: ", c.get_motor()," ", c.get_rodas()," ", c.get_volante()," ", c.get_marca()));
