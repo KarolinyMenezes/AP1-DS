6004ad16203717f8c0b867fff21e3b4729a8bf44
KarolinyMenezes <karoliny.menezess@gmail.com>
2026-04-10 15:29:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+
+Lanche l = new Lanche();
+
+Console.WriteLine("Tamanho do pão?");
+l.set_pão(Console.ReadLine());
+
+Console.WriteLine("Qual molho?");
+l.set_molho(Console.ReadLine());
+
+Console.WriteLine("Qual carne?");
+l.set_carne(Console.ReadLine());
+
+Console.WriteLine("Com salada?");
+l.set_salada(Console.ReadLine());
+
+Console.WriteLine(string.Concat("Seu lanche é: ", l.get_pão(), " ", l.get_molho(), " ", l.get_carne(), " ", l.get_salada()));
