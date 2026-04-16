c0b5ac54ddd3050bba1a5875774831b786f91c56
KarolinyMenezes <karoliny.menezess@gmail.com>
2026-04-16 15:15:11 -0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
+Filme f = new Filme();
+
+Console.WriteLine("Quais falas?");
+f.set_falas(Console.ReadLine());
+
+Console.WriteLine("Quais ações?");
+f.set_açoes(Console.ReadLine());
+
+Console.WriteLine("Qual gênero?");
+f.set_genero(Console.ReadLine());
+
+Console.WriteLine("Qual classificação?");
+f.set_classificaçao(Console.ReadLine());
+
+Console.WriteLine(string.Concat("Seu filme é: ", f.get_falas(), " ", f.get_açoes(), " ", f.get_genero(), " ", f.get_classificaçao()));
