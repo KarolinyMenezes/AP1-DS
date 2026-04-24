006ad28af569cbf917022bc812f4b2c4752fe4a4
KarolinyMenezes <karoliny.menezess@gmail.com>
2026-04-24 17:26:53 -0300

    Add files via upload

---
+++ exercicio5/Paciente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    internal class Paciente
+    {
+        private string _nome;
+        private string _CPF;
+        private string _senhaDeChamada;
+        private string _sintomas;
+
+        public Paciente(string nome, string CPF, string senhaDeChamada, string sintomas)
+        {
+            this._nome = nome;
+            this._CPF = CPF;
+            this._senhaDeChamada = senhaDeChamada;
+            this._sintomas = sintomas;
+        }
+        public Paciente()
+        {
+        }
+        public string get_nome()
+        {
+            return this._nome;
+        }
+        public void set_nome(string nome)
+        {
+            this._nome = nome;
+        }
+        public string get_CPF()
+        {
+            return this._CPF;
+        }
+        public void set_CPF(string CPF)
+        {
+            this._CPF = CPF;
+        }
+        public string get_senhaDeChamada()
+        {
+            return this._senhaDeChamada;
+        }
+        public void set_senhaDeChamada(string senhaDeChamada)
+        {
+            this._senhaDeChamada = senhaDeChamada;
+        }
+        public string get_sintomas()
+        {
+            return this._sintomas;
+        }
+        public void set_sintomas(string sintomas)
+        {
+            this._sintomas = sintomas;
+        }
+    }
+}
---
+++ exercicio5/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp5;
+
+Paciente p = new Paciente();
+
+Console.WriteLine("Nome do paciente?");
+p.set_nome(Console.ReadLine());
+
+Console.WriteLine("Qual o CPF?");
+p.set_CPF(Console.ReadLine());
+
+Console.WriteLine("Qual a senha de chamada?");
+p.set_senhaDeChamada(Console.ReadLine());
+
+Console.WriteLine("Quais os sintomas?");
+p.set_sintomas(Console.ReadLine());
+
+Console.WriteLine(string.Concat("Seu Paciente é: ", p.get_nome(), " ", p.get_CPF(), " ", p.get_senhaDeChamada(), " ", p.get_sintomas()));
